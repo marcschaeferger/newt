22e5104a411d9ff83643fb5d06f87d0f0b4e34c7
Varun Narravula <varun@snare.dev>
2025-12-11 17:50:16 -0800

    fix(nix): use correct hash for vendored deps

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
             inherit version;
             src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
 
-            vendorHash = "sha256-krxkfH+4z0rgmFi8OyCzWIrxU5Rpb7gjtGcn3LnZCig=";
+            vendorHash = "sha256-5Xr6mwPtsqEliKeKv2rhhp6JC7u3coP4nnhIxGMqccU=";
 
             env = {
               CGO_ENABLED = 0;
