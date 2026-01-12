b84d4657634293a3aa567f7472d1ef5daa5fcc78
Owen <owen@txv.io>
2026-01-12 12:31:38 -0800

    Add noop for android ios

--- network/interface.go
+++ network/interface.go
@@ -44,9 +44,13 @@ func ConfigureInterface(interfaceName string, tunnelIp string, mtu int) error {
 		return configureDarwin(interfaceName, ip, ipNet)
 	case "windows":
 		return configureWindows(interfaceName, ip, ipNet)
-	default:
-		return fmt.Errorf("unsupported operating system: %s", runtime.GOOS)
+	case "android":
+		return nil
+	case "ios":
+		return nil
 	}
+	
+	return nil
 }
 
 // waitForInterfaceUp polls the network interface until it's up or times out
